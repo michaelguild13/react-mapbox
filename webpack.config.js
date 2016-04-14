ec625c3cdae820afa627dfc421b50b2222d59878
Michael Guild <michaelguild13@gmail.com>
2016-04-14 07:06:26 -0400

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 // https://webpack.github.io/docs/webpack-dev-server.html
 var webpack = require('webpack'),
-    path = require('path');
+    path = require('path'),
+    fs = require('fs');
+
+// Don't include nodeModules
+// We need this so that webpack-dev-server doesn't complain when compiling
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+      return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+      nodeModules[mod] = 'commonjs ' + mod;
+  });
+//console.log('Node Modules: '+ JSON.stringify(nodeModules));
 
 module.exports = {
-  externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM'
-  },
+  externals: nodeModules,
   module: {
     loaders: [
       {
